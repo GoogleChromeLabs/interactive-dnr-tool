431a9a393b4f36170aea099bdc13a0ab6eb37a28
hrmtsh2 <amiteshm0101@gmail.com>
2024-07-23 20:36:36 +0530

    Modify priority resolution and add to rulesStore.js

--- src/stores/rulesStore.js
+++ src/stores/rulesStore.js
@@ -1,6 +1,6 @@
-import { defineStore } from 'pinia'
-import { useURLFilterStore } from './urlFilterStore'
-import { isValidURLFilter } from '@/utils'
+import { defineStore } from 'pinia';
+import { useURLFilterStore } from './urlFilterStore';
+import { isValidURLFilter, getHighestPriorityRules } from '@/utils';
 
 // const urlFilterStore = useURLFilterStore()
 
@@ -12,25 +12,25 @@ export const useRulesStore = defineStore('rules', {
   }),
   getters: {
     getParsedRulesList(state) {
-      return state.parsedRulesList
+      return state.parsedRulesList;
     }
   },
   actions: {
     setRulesetFilesUploaded(value) {
-      this.rulesetFilesUploaded = value
+      this.rulesetFilesUploaded = value;
     },
     // Checks validity of URLFilter string
     isValidURLFilter,
     // Checks validity of rule, including checking validity of its condition, i.e., the URLFilter string
     isValidRule(rule) {
-      let isValid = true
+      let isValid = true;
       if (!rule.id || (rule.id && !Number.isInteger(rule.id))) {
-        isValid = false
-        console.log('id')
+        isValid = false;
+        console.log('id');
       }
       if (rule.priority && !Number.isInteger(rule.priority)) {
-        isValid = false
-        console.log('priority')
+        isValid = false;
+        console.log('priority');
       }
       if (
         !rule.action ||
@@ -38,66 +38,70 @@ export const useRulesStore = defineStore('rules', {
         !rule.action.type ||
         (rule.action && typeof rule.action.type != 'string')
       ) {
-        isValid = false
-        console.log('action')
+        isValid = false;
+        console.log('action');
       }
       if (!rule.condition || typeof rule.condition != 'object') {
-        isValid = false
-        console.log('condition')
+        isValid = false;
+        console.log('condition');
       }
       if (!this.isValidURLFilter(rule.condition.urlFilter)) {
-        isValid = false
+        isValid = false;
       }
-      return isValid
+      return isValid;
     },
     // Checks syntax and validity of ruleset file
     isValidRuleset(ruleset) {
       // Check if the ruleset is an array and is non-empty
       if (!Array.isArray(ruleset) || ruleset.length === 0) {
-        return false
+        return false;
       }
 
       // Validate each rule in the ruleset
       for (let rule of ruleset) {
         if (!this.isValidRule(rule)) {
-          return false
+          return false;
         }
       }
 
-      return true
+      return true;
     },
     // Test input url against the generated indexedRule object
     urlMatcher(url, indexedRule) {
-      const urlPattern = indexedRule.urlPattern
+      const urlPattern = indexedRule.urlPattern;
 
       if (indexedRule.urlPatternType === 'SUBSTRING') {
         if (!url.includes(urlPattern)) {
-          return false
+          return false;
         } else {
-          return true
+          return true;
         }
       } else if (indexedRule.urlPatternType === 'WILDCARDED') {
-        let substrings = []
-        let string = ''
+        let substrings = [];
+        let string = '';
         for (let i = 0; i < urlPattern.length; i++) {
-          if (urlPattern[i] === '*' || urlPattern[i] === '^' || urlPattern[i] === '|') {
+          if (
+            urlPattern[i] === '*' ||
+            urlPattern[i] === '^' ||
+            urlPattern[i] === '|'
+          ) {
             if (string) {
-              substrings.push(string)
-              string = ''
+              substrings.push(string);
+              string = '';
             }
           } else {
-            string += urlPattern[i]
+            string += urlPattern[i];
           }
         }
-        substrings.push(string)
-        let x = 0 // index in urlPattern
-        let index // index in url where the checking starts
+        substrings.push(string);
+        let x = 0; // index in urlPattern
+        let index; // index in url where the checking starts
         if (indexedRule.anchorLeft === 'BOUNDARY') {
-          index = 0
+          index = 0;
         } else {
-          index = url.indexOf(substrings[0])
+          index = url.indexOf(substrings[0]);
           if (index == -1) {
-            return false
+            return false;
           }
         }
         if (indexedRule.anchorRight === 'BOUNDARY') {
@@ -106,38 +110,41 @@ export const useRulesStore = defineStore('rules', {
             urlPattern[urlPattern.length - 1] != '*' &&
             url.endsWith(substrings[substrings.length - 1])
           ) {
-            return true
+            return true;
           }
         }
         if (indexedRule.anchorLeft === 'SUBDOMAIN') {
-          index = url.indexOf(substrings[0])
+          index = url.indexOf(substrings[0]);
           if (index == -1) {
-            return false
+            return false;
           }
         }
-        let unmatchables = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.%'
+        let unmatchables =
+          'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.%';
         // DOUBT: Can multiple wildcards be present in the urlPattern?
         while (urlPattern[x] == '*') {
-          x++
+          x++;
         }
         while (urlPattern[x] == '^') {
           if (unmatchables.includes(url[index])) {
-            return false
+            return false;
           }
-          x++
-          index++
+          x++;
+          index++;
         }
-        let inOrder = url.indexOf(substrings[0], index) != -1
+        let inOrder = url.indexOf(substrings[0], index) != -1;
         for (let i = 1; i < substrings.length; i++) {
-          if (url.indexOf(substrings[i], url.indexOf(substrings[i - 1])) == -1) {
-            inOrder = false
-            break
+          if (
+            url.indexOf(substrings[i], url.indexOf(substrings[i - 1])) == -1
+          ) {
+            inOrder = false;
+            break;
           }
         }
         if (inOrder) {
-          return true
+          return true;
         } else {
-          return false
+          return false;
         }
       }
     },
@@ -149,17 +156,32 @@ export const useRulesStore = defineStore('rules', {
     */
     setParsedRulesList(rulesetObject) {
       rulesetObject.ruleset.forEach((rule) => {
-        const ruleID = rule.id
+        const ruleID = rule.id;
         if (this.isValidRule(rule)) {
-          let indexedRule = this.urlFilterStore.parseURLFilter(rule.condition.urlFilter)
+          let indexedRule = this.urlFilterStore.parseURLFilter(
+            rule.condition.urlFilter
+          );
           this.parsedRulesList.push({
             rule: rule,
             urlParserIndexedRule: indexedRule,
             ruleId: ruleID,
             rulesetId: rulesetObject.rulesetId
-          })
+          });
         }
-      })
+      });
+    },
+    requestMatcher(request) {
+      let url = request.url;
+      let ruleset = this.parsedRulesList;
+      let matchedRules = [];
+      for (let i = 0; i < ruleset.length; i++) {
+        let indexedRule = ruleset[i].urlParserIndexedRule;
+        if (this.urlMatcher(url, indexedRule)) {
+          matchedRules.push(ruleset[i]);
+        }
+      }
+      let highestPriorityRules = getHighestPriorityRules(matchedRules);
+      return highestPriorityRules;
     }
   }
-})
+});
--- src/utils.js
+++ src/utils.js
@@ -1,40 +1,119 @@
 // Checks validity of URLFilter string
-export function isValidURLFilter(urlFilterString) {
+function isValidURLFilter(urlFilterString) {
   // Ensure only valid constructs are present
-  const validConstructs = /^[*|^a-zA-Z0-9_\-.%?/;=@&:]+$/
+  const validConstructs = /^[*|^a-zA-Z0-9_\-.%?/;=@&:]+$/;
   if (!validConstructs.test(urlFilterString)) {
-    return false // Invalid constructs present
+    return false; // Invalid constructs present
   }
   // Check for ASCII characters
   for (let i = 0; i < urlFilterString.length; i++) {
     if (urlFilterString.charCodeAt(i) > 127) {
-      return false // Non-ASCII character found
+      return false; // Non-ASCII character found
     }
   }
   if (urlFilterString.startsWith('||*')) {
-    return false // Cannot start with ||* - use only * at the beginning for the intended effect.
+    return false; // Cannot start with ||* - use only * at the beginning for the intended effect.
   }
   if (urlFilterString.includes('|') && !urlFilterString.includes('||')) {
     if (!urlFilterString.startsWith('|') && !urlFilterString.endsWith('|')) {
-      return false // Cannot start or end without |.
+      return false; // Cannot start or end without |.
     }
     if (urlFilterString.length === 1) {
-      return false // Cannot have only |.
+      return false; // Cannot have only |.
     }
   }
   if (urlFilterString.includes('||')) {
     if (!urlFilterString.startsWith('||')) {
-      return false // Cannot have || in the middle.
+      return false; // Cannot have || in the middle.
     }
     if (urlFilterString.length === 2) {
-      return false // Cannot have only ||.
+      return false; // Cannot have only ||.
     }
-    if (urlFilterString.slice(2).includes('|') && !urlFilterString.endsWith('|')) {
-      return false // Cannot have | in the middle.
+    if (
+      urlFilterString.slice(2).includes('|') &&
+      !urlFilterString.endsWith('|')
+    ) {
+      return false; // Cannot have | in the middle.
     }
   }
-  if (urlFilterString.slice(1, -1).includes('|') && urlFilterString.slice(0, 2) !== '||') {
-    return false // Cannot have | in the middle.
+  if (
+    urlFilterString.slice(1, -1).includes('|') &&
+    urlFilterString.slice(0, 2) !== '||'
+  ) {
+    return false; // Cannot have | in the middle.
   }
-  return true
+  return true;
 }
+
+function getHighestPriorityRules(matchedRulesList) {
+  // priority based on priority value
+  const highestPriority = Math.max(
+    ...matchedRulesList.map((rule) => rule.rule.priority)
+  );
+  let highestPriorityRules = matchedRulesList.filter(
+    (rule) => rule.rule.priority === highestPriority
+  );
+  if (highestPriorityRules.length === 1) {
+    return highestPriorityRules;
+  }
+  // priority based on action type
+  const actionByPriority = [
+    'allow',
+    'allowAllRequests',
+    'block',
+    'upgradeScheme',
+    'redirect',
+    'modifyHeaders'
+  ]; // 'modifyHeaders' dealt with differently
+  const highestPriorityAction = Math.max(
+    ...highestPriorityRules.map((rule) =>
+      actionByPriority.indexOf(rule.rule.action.type)
+    )
+  );
+  highestPriorityRules = highestPriorityRules.filter(
+    (rule) => rule.rule.action.type === actionByPriority[highestPriorityAction]
+  );
+  // If priority is not block or redirect, and a 'modifyHeaders' rule exists
+  let modifyHeadersRules = matchedRulesList.filter(
+    (rule) => rule.rule.action.type === 'modifyHeaders'
+  );
+  if (
+    actionByPriority[highestPriorityAction] !== 'block' &&
+    actionByPriority[highestPriorityAction] !== 'redirect' &&
+    modifyHeadersRules.length !== 0
+  ) {
+    // Check for allow or allowAllRequests rule
+    let allowRules = highestPriorityRules.filter(
+      (rule) =>
+        rule.rule.action.type === 'allow' ||
+        rule.rule.action.type === 'allowAllRequests'
+    );
+    // Find highest priority allow or allowAllRequests rule
+    let highestAllowPriority = -1;
+    let eligibleModifyHeadersRules = [];
+    if (allowRules.length > 0) {
+      highestAllowPriority = Math.max(
+        ...allowRules.map((rule) => rule.rule.priority)
+      );
+      // Then find all modifyHeaders rules with the same priority or higher
+      eligibleModifyHeadersRules = modifyHeadersRules.filter(
+        (rule) => rule.rule.priority <= highestAllowPriority
+      );
+    } else {
+      eligibleModifyHeadersRules = modifyHeadersRules;
+    }
+    // Then find the highest priority modifyHeaders rule based on dev defined priority
+    const highestModifyHeadersPriority = Math.max(
+      ...eligibleModifyHeadersRules.map((rule) => rule.rule.priority)
+    );
+    let highestModifyHeadersRules = eligibleModifyHeadersRules.filter(
+      (rule) => rule.rule.priority === highestModifyHeadersPriority
+    );
+    // If there is only one highest priority modifyHeaders rule, return it
+    highestPriorityRules = highestModifyHeadersRules;
+    // TODO: If not, check the type of headers modified
+  }
+  return highestPriorityRules;
+}
+
+export { isValidURLFilter, getHighestPriorityRules };
