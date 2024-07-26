1fc2b795205309b51cfd35f8ad7efcc81c13e97e
hrmtsh2 <amiteshm0101@gmail.com>
2024-07-26 21:14:02 +0530

    Fix urlMatcher

--- src/stores/urlFilterStore.js
+++ src/stores/urlFilterStore.js
@@ -1,6 +1,15 @@
 import { defineStore } from 'pinia';
 import { isValidURLFilter } from '@/utils';
 
+//https://source.chromium.org/chromium/chromium/src/+/main:extensions/browser/api/declarative_net_request/indexed_rule.cc;l=47
+/* Returns an object 'indexedRule' with the following signature:-
+    {
+      anchorLeft: 'BOUNDARY' | 'SUBDOMAIN' | 'NONE',
+      urlPatternType: 'SUBSTRING' | 'WILDCARDED',
+      urlPattern: 'abc*def',
+      anchorRight: 'BOUNDARY' | 'NONE'
+    }
+*/
 class URLFilterParser {
   constructor(urlFilter, indexedRule) {
     this.urlFilter = urlFilter || '';
@@ -117,13 +126,12 @@ export const useURLFilterStore = defineStore('urlFilter', {
     // Test input url against the generated indexedRule object
     urlMatcher(url, indexedRule) {
       const urlPattern = indexedRule.urlPattern;
-
+      let substrings = [];
       if (indexedRule.urlPatternType === 'SUBSTRING') {
         if (!url.includes(urlPattern)) {
           return false;
-        } else {
-          return true;
         }
+        substrings.push(urlPattern);
       } else if (indexedRule.urlPatternType === 'WILDCARDED') {
         let substrings = [];
         let string = '';
@@ -142,58 +150,56 @@ export const useURLFilterStore = defineStore('urlFilter', {
           }
         }
         substrings.push(string);
-        let x = 0; // index in urlPattern
-        let index; // index in url where the checking starts
-        if (indexedRule.anchorLeft === 'BOUNDARY') {
-          index = 0;
-        } else {
-          index = url.indexOf(substrings[0]);
-          if (index == -1) {
-            return false;
-          }
-        }
-        if (indexedRule.anchorRight === 'BOUNDARY') {
-          if (
-            urlPattern[urlPattern.length - 1] != '^' &&
-            urlPattern[urlPattern.length - 1] != '*' &&
-            url.endsWith(substrings[substrings.length - 1])
-          ) {
-            return true;
-          }
-        }
-        if (indexedRule.anchorLeft === 'SUBDOMAIN') {
-          index = url.indexOf(substrings[0]);
-          if (index == -1) {
-            return false;
-          }
-        }
-        let unmatchables =
-          'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.%';
-        // DOUBT: Can multiple wildcards be present in the urlPattern?
-        while (urlPattern[x] == '*') {
-          x++;
-        }
-        while (urlPattern[x] == '^') {
-          if (unmatchables.includes(url[index])) {
-            return false;
-          }
-          x++;
-          index++;
-        }
-        let inOrder = url.indexOf(substrings[0], index) != -1;
-        for (let i = 1; i < substrings.length; i++) {
-          if (
-            url.indexOf(substrings[i], url.indexOf(substrings[i - 1])) == -1
-          ) {
-            inOrder = false;
-            break;
-          }
-        }
-        if (inOrder) {
-          return true;
-        } else {
+      }
+      let x = 0; // index in urlPattern
+      let index; // index in url where the checking starts
+      if (indexedRule.anchorLeft === 'BOUNDARY') {
+        index = 0;
+      } else {
+        index = url.indexOf(substrings[0]);
+        if (index == -1) {
           return false;
         }
+      }
+      if (indexedRule.anchorRight === 'BOUNDARY') {
+        if (
+          urlPattern[urlPattern.length - 1] != '^' &&
+          urlPattern[urlPattern.length - 1] != '*' &&
+          url.endsWith(substrings[substrings.length - 1])
+        ) {
+          return true;
+        }
+      }
+      if (indexedRule.anchorLeft === 'SUBDOMAIN') {
+        index = url.indexOf(substrings[0]);
+        if (index == -1 || url[index - 1] != '.') {
+          return false;
+        }
+      }
+      let unmatchables =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.%';
+      // DOUBT: Can multiple wildcards be present in the urlPattern?
+      while (urlPattern[x] == '*') {
+        x++;
+      }
+      while (urlPattern[x] == '^') {
+        if (unmatchables.includes(url[index])) {
+          return false;
+        }
+        x++;
+        index++;
+      }
+      let inOrder = url.indexOf(substrings[0], index) != -1;
+      for (let i = 1; i < substrings.length; i++) {
+        if (url.indexOf(substrings[i], url.indexOf(substrings[i - 1])) == -1) {
+          inOrder = false;
+          break;
+        }
+      }
+      if (inOrder) {
+        return true;
+      } else {
+        return false;
       }
     }
   }
